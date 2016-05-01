3d77b4d3a6cb901ec6e6f836464e0dfd29bacbec
Daniel Häfele <haefele@xemio.net>
2016-05-01 12:14:12 +0200

    Add logout hamburger button and correctly restore application mode

--- src/App/SBoard/App.xaml.cs
+++ src/App/SBoard/App.xaml.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using Caliburn.Micro;
 using SBoard.ApplicationModes;
 using SBoard.Core.Common;
+using SBoard.Core.Services.ApplicationState;
 using SBoard.Core.Services.Centron;
 using SBoard.Strings;
 using SBoard.Views.Login;
 using UwCore.Application;
+using UwCore.Services.ApplicationState;
 
 namespace SBoard
 {
@@ -15,11 +17,6 @@ namespace SBoard
         public App()
         {
             this.InitializeComponent();
-        }
-
-        public override ApplicationMode GetCurrentMode()
-        {
-            return IoC.Get<LoggedOutApplicationMode>();
         }
          
         public override IEnumerable<Type> GetViewModelTypes()
@@ -41,5 +38,24 @@ namespace SBoard
         public override string GetErrorMessage() => SBoardResources.Get("Errors.Message");
 
         public override Type GetCommonExceptionType() => typeof(SBoardException);
+        
+        public override ApplicationMode GetCurrentMode()
+        {
+            var state = IoC.Get<IApplicationStateService>();
+            string webServiceAddress = state.GetWebServiceAddress();
+            string username = state.GetUsername();
+            string password = state.GetPassword();
+
+            if (string.IsNullOrWhiteSpace(webServiceAddress) == false &&
+                string.IsNullOrWhiteSpace(username) == false &&
+                string.IsNullOrWhiteSpace(password) == false)
+            {
+                return IoC.Get<LoggedInApplicationMode>();
+            }
+            else
+            {
+                return IoC.Get<LoggedOutApplicationMode>();
+            }
+        }
     }
 }
--- src/App/SBoard/ApplicationModes/LoggedInApplicationMode.cs
+++ src/App/SBoard/ApplicationModes/LoggedInApplicationMode.cs
@@ -1,15 +1,42 @@
-﻿using UwCore.Application;
+﻿using Caliburn.Micro;
+using SBoard.Common;
+using SBoard.Core.Services.ApplicationState;
+using SBoard.Strings;
+using UwCore.Application;
+using UwCore.Hamburger;
+using UwCore.Services.ApplicationState;
 
 namespace SBoard.ApplicationModes
 {
     public class LoggedInApplicationMode : ApplicationMode
     {
+        private readonly IApplicationStateService _applicationStateService;
+
+        private readonly ClickableHamburgerItem _logoutItem;
+
+        public LoggedInApplicationMode(IApplicationStateService applicationStateService)
+        {
+            this._applicationStateService = applicationStateService;
+
+            this._logoutItem = new ClickableHamburgerItem(SBoardResources.Get("Logout"), SymbolEx.Logout, this.Logout);
+        }
+
         public override void Enter()
         {   
+            this.Application.SecondaryActions.Add(this._logoutItem);
         }
 
         public override void Leave()
         {
+            this.Application.SecondaryActions.Remove(this._logoutItem);
+        }
+
+        private async void Logout()
+        {
+            this._applicationStateService.SetPassword(string.Empty);
+            await this._applicationStateService.SaveStateAsync();
+
+            this.Application.CurrentMode = IoC.Get<LoggedOutApplicationMode>();
         }
     }
 }
